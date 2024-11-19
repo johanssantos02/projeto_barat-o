ae7ad96aa215fe59f5c748784ecbfde8b7ed7fef
Johan Silva Cardoso <142803636+johanssantos02@users.noreply.github.com>
2024-11-19 18:27:49 -0300

    atualização

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(function(){
+// $(function(){
 
-$('#b2').click(function(){
-    $('.landing').css('background-image' , "url(imagens/banner1.png)");
-    $('.landing').css('background-size' , "cover")
-})
-$('#b1').click(function(){
-    $('.landing').css('background-image' , "url(imagens/vamos-resolver.png)").css('background-size' , 'contain'), 'background-repeat', 'no-repeat' , 'background-position' , 'center')
-})
+// $('#b2').click(function(){
+//     $('.landing').css('background-image' , "url(imagens/banner1.png)");
+//     $('.landing').css('background-size' , "cover")
+// })
+// $('#b1').click(function(){
+//     $('.landing').css('background-image' , "url(imagens/vamos-resolver.png)").css('background-size' , 'contain'), 'background-repeat', 'no-repeat' , 'background-position' , 'center')
+// })
 
 
 
-})
+// })
