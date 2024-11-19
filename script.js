fe753bed119cbd7e97ca4eedf91f097c3d0e5891
Johan Silva Cardoso <142803636+johanssantos02@users.noreply.github.com>
2024-11-19 13:59:09 -0300

    primeira atualização

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+$('#b2').click(function(){
+    $('.landing').css('background-image' , "url(imagens/banner1.png)");
+    $('.landing').css('background-size' , "cover")
+})
+$('#b1').click(function(){
+    $('.landing').css('background-image' , "url(imagens/vamos-resolver.png)").css('background-size' , 'contain'), 'background-repeat', 'no-repeat' , 'background-position' , 'center')
+})
+
+
+
+})
